c6e95d4fc3dd284408bc0c8ee56cc82a27db7abe
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-05-05 19:46:01 +0530

    init final assignment

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Getting-and-Cleaning-Data-Course-Project
-A peer graded assignment for week-4
+
+### Peer-graded Assignment: Getting and Cleaning Data Course Project
+
+This repository holds the peer graded assignment for week-4, as in the course **Getting and Cleaning Data**, under the **Data Science in R** specialization. It consists of the instructions about the execution of the analysis on Human Activity recognition dataset.
+
+### Dataset Analysed
+
+The dataset, that is used for this analysis, is available at the following link: <br />
+[Human Activity Recognition Using Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+### Files Included in the Repo.
+
+* ```CodeBook.md``` a code book that describes the variables, the data, and any transformations > or work that I performed to clean up the data <br />
+* ```run_analysis.R``` performs the data preparation and then followed by the 5 steps required > as described in the course project’s definition:  <br />
+>   * Merges the training and the test sets to create one data set.
+>   * Extracts only the measurements on the mean and standard deviation for each measurement.
+>   * Uses descriptive activity names to name the activities in the data set
+>   * Appropriately labels the data set with descriptive variable names.
+>   * From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+* ```UCI HAR Dataset``` directory holds the downloaded dataset (as from the source, provided).
+
+* ```FinalData.txt``` is the exported final data after going through all the sequences described above.
+
+<br />
+
+(A compressed ``` .zip``` file is also included, holding data files)
+
+<br /><hr />
+
+_This submission is made by [Ravi Prakash](https://raviprakashravi.cf/)!_
+
+
---
+++ assignment.R
@@ -0,0 +1,103 @@
+# loading required libraries
+library(dplyr)
+
+
+
+###############################
+
+##    PREAPARING THE DATASETs
+
+###############################
+
+# downloading the datasets that has to be used
+filename <- "Coursera_DS3_Final.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+
+# loading the files into data-frame
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+
+
+
+###########    SOLUTION    ###########
+######################################
+
+##    TRANSFORMING THE DATASETs     ##
+
+######################################
+
+# Step 1 #
+# MERGGING 2 datasets
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+
+# Step 2 #
+# Extracting measurements on the mean and standard deviation
+TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+
+
+# Step 3 #
+# Attributes' name transformed in order to name the activities present in the dataset
+TidyData$code <- activities[TidyData$code, 2]
+
+
+# Step 4 #
+# Labeling the data set with descriptive variable names
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+
+# Step 5 #
+# Creating the final (second independent) tidy dataset with average of each variable for each activity and each subject
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
+
+
+#############################################
+#############################################
+
+
+# checking the structure
+str(FinalData)
+
+# Viewing the results
+View(FinalData)
+
+
+
+
+
